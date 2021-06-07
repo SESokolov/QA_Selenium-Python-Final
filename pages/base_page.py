2fef4ef46b821f8b7cbc677e6a19358082660315
Соколов Сергей <admin@zdn.limak.ru>
2021-06-07 11:37:20 +0300

    Переписал тест из файла test_main_page.py с помощью паттерна Page Object: каталог pages

---
+++ pages/base_page.py
@@ -0,0 +1,8 @@
+﻿# Базовяа страница, от которой будут унаследованы все остальные классы. В ней мы опишем вспомогательные методы для работы с драйвером.
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
