ac5627c94d4a93e07857b8b3e7d71855c0265b21
Соколов Сергей <admin@zdn.limak.ru>
2021-06-07 15:19:50 +0300

    Добавил проверку login page

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_URL = "login"
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
