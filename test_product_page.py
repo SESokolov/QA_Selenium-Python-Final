fd8e6147319ee66a7e634e68fe49194726ee63b6
Соколов Сергей <admin@zdn.limak.ru>
2021-06-18 13:20:39 +0300

    Добавил решение шага 4.3.2, работает и на 4.3.3 https://stepik.org/lesson/201964/step/2?unit=176022

---
+++ test_product_page.py
@@ -0,0 +1,7 @@
+﻿from pages.product_page import ProductPage
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.add_product_to_basket()          # выполняем метод страницы — добавляем товар в корзину
